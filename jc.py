9f8257024969db7be5e86f4693fcf459317edc88
Michiel <msvos@liacs.nl>
2015-06-18 17:27:08 +0200

    perm heeft duplicate locaties

--- jc.py
+++ jc.py
@@ -1,5 +1,6 @@
 from math import floor
 
+"""
 factorial = [
    1,
    26,
@@ -21,7 +22,6 @@ factorial = [
     7731052560,
     316973154960
 ]
-"""
 
 skip_right = [
     0,   1,  2,
@@ -67,55 +67,78 @@ use_dens = [
 def unrank(N, K, r, perm):
     perm_index = [0] * K
     
-    perm_index[0] = int(r % 26)
-    r = floor(r/26)
-    perm[0] = perm_index[0]
-    perm[0] = skip_right[perm[0]]
+    #perm_index[0] = int(r % 26)
+    #r = floor(r/26)
+    #perm[0] = perm_index[0]
+    #perm[0] = skip_right[perm[0]]
     
-    for k in range(1, K):
+    #perm_index[0] = int(r % 47)
+    #r = floor(r/47)
+    #perm[0] = perm_index[0]
+
+    for k in range(0, K):
         perm_index[k] = int(r % (N-k))
         perm[k] = perm_index[k]
         for i in range(0, k):
             if perm_index[i] <= perm_index[k]:
                 perm[k] += 1
-        perm[k] = skip_dens[perm[k]]
+        #perm[k] = skip_dens[perm[k]]
         r = floor(r/(N-k))
+
+    #print 'perm_index: ', perm_index
+    #print 'perm: ', perm
 
 
 def rank(N, K, perm):
-    perm[0] = use_right[perm[0]]
+    #perm[0] = use_right[perm[0]]
     r = perm[0]
-    for k in range(1, K):
-        perm[k] = use_dens[perm[k]]
+    #for k in range(1, K):
+    #    perm[k] = use_dens[perm[k]]
 
     for k in range(1, K)[::-1]:
         q = perm[k]
         for i in range(0, k):
             if perm[i] < perm[k]:
                 q -= 1
-        r += q * factorial[k]
+        #r += q * factorial[k]
+        r += q * fac[N]/fac[N-k]
     return r
 
 def main():
     print 'Jungle Checkers ranking'
 
-    N = 47 # Every digit in range [0, N-1]
+    N = 4 # Every digit in range [0, N-1]
     K = 3 # Permutation length
 
+    # Precalculate faculty
+    global fac
+    fac = [0] * (N+1)
+    fac[0] = 1
+    fac[1] = 1
+    for i in range(2, N+1):
+        fac[i] = i * fac[i-1]
+
+
     # Loop thru all permutations and test rank/unrank
-    for index in range(0, factorial[K]-1):
-            perm = [-1] * K
-            unrank(N, K, index, perm)
-            r = rank(N, K, perm)
-            if index != r:
-                print index, '\t', perm, '\t', r
+    #"""
+    #for index in range(0, factorial[K]-1):
+    for index in range(0, fac[N]/fac[N-K]-1):
+        perm = [-1] * K
+        unrank(N, K, index, perm)
+        r = rank(N, K, perm)
+        if index != r:
+            print index, '\t', perm, '\t', r
+            break
+        print index, '\t', perm, '\t', r
+
     """
-    index = 51
+    index = 1
     perm = [-1] * K
     unrank(N, K, index, perm)
     r = rank(N, K, perm)
     print index, '\t', perm, '\t', r
     
+     " ""
     index = 52
     perm = [-1] * K
     unrank(N, K, index, perm)
@@ -127,8 +150,8 @@ def main():
     unrank(N, K, index, perm)
     r = rank(N, K, perm)
     print index, '\t', perm, '\t', r
-    """
 
+    """
 
 
     print 'done'
