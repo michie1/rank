af23407a884e36d2f7737e12f89ba925718c5a8d
Michiel <msvos@liacs.nl>
2015-06-25 17:12:21 +0200

    renamed and divided a lot of methods

--- jc.py
+++ jc.py
@@ -64,6 +64,7 @@ use_dens = [
 ]
 
 
+# unrank by rank
 def unrank(N, K, r, perm):
     perm_index = [0] * K
     
@@ -72,18 +73,10 @@ def unrank(N, K, r, perm):
     perm[0] = perm_index[0]
     perm[0] = skip_right[perm[0]]
     
-    #perm_index[0] = int(r % 47)
-    #r = floor(r/47)
-    #perm[0] = perm_index[0]
-
-    #for k in range(0, K):
     for k in range(1, K):
         perm_index[k] = int(r % (N-k))
         r = floor(r/(N-k))
 
-    #print perm_index
-
-    #for k in range(0, K):
     for k in range(1, K):
         perm[k] = perm_index[k]
         i = k-1
@@ -93,14 +86,12 @@ def unrank(N, K, r, perm):
             i -= 1
         perm[k] = skip_dens[perm[k]]
 
-    #print perm
-
-    #print 'perm_index: ', perm_index
-    #print 'perm: ', perm
+# unrank to index
+#def unrank_to_index(N, K, r):
 
 
+# rank by perm
 def rank(N, K, perm):
-    #print perm
     perm[0] = use_right[perm[0]]
     r = perm[0]
     for k in range(1, K):
@@ -112,23 +103,35 @@ def rank(N, K, perm):
             if perm[i] < perm[k]:
                 q -= 1
         r += q * factorial[k]
-        #r += q * fac[N]/fac[N-k]
     return r
 
-def next_rank(N, K, perm, perm_index):
-    #print perm_index, 
-    """
-    perm_index[0] += 1
-    if perm_index[0] == N:
-        perm_index[0] = 0
-        perm_index[1] += 1
-        if perm_index[1] == N-1:
-            perm_index[1] = 0
-            perm_index[2] += 1
-    """
+# rank by index
+def index_to_rank(N, K, perm_index):
+    r = perm_index[0]
+    for k in range(1, K): 
+        r += perm_index[k] * factorial[k]
+    return r
 
+def rank_to_index(r, perm_index):
+    perm_index[0] = int(r % 26)
+    r = floor(r/26)
+
+    for k in range(1, K):
+        perm_index[k] = int(r % (47-k))
+        r = floor(r / (47-k))
+
+# next index
+def increment_index(N, K, perm_index):
+    # increment index
+    """
     perm_index[0] += 1
     if perm_index[0] == 26:
+        perm_index[0] = 0
+        perm_index[1] += 1
+    """
+    if perm_index[0] < 25:
+        perm_index[0] += 1
+    else: # == 25
         perm_index[0] = 0
         perm_index[1] += 1
 
@@ -138,9 +141,8 @@ def next_rank(N, K, perm, perm_index):
         perm_index[i+1]+= 1
         i += 1
 
-    #print perm_index
-
-    #perm = [0]*K
+def index_to_perm(N, K, perm, perm_index):
+    # Get perm by index
     for k in range(0, K):
         perm[k] = perm_index[k]
         i = k-1
@@ -149,13 +151,36 @@ def next_rank(N, K, perm, perm_index):
                 perm[k] += 1
             i -= 1
 
-    #print perm_index, perm
+    # transform to game
+    perm[0] = skip_right[perm[0]]
+    for k in range(1, K):
+        perm[k] = skip_dens[perm[k]]
+
+def perm_to_index(N, K, perm, perm_index):
+    # transform from game
+    perm_index[0] = use_right[perm[0]]
+    for k in range(1, K):
+        perm_index[k] = use_dens[perm[k]]
+    
     #print perm
-    #return perm
+    #perm_index[0] = skip_right[perm[0]]
+    #for k in range(1, K):
+    #    perm_index[k] = skip_dens[perm[k]]
+
+    # to index
+    for k in range(0, K)[::-1]:
+    #for k in range(1, K)[::-1]:
+        i = k-1
+        while i >= 0:
+            if perm_index[k] > perm_index[i]:
+                perm_index[k] -= 1
+            i -= 1
+        #perm_index[k] = use_dens[perm[k]]
 
 def main():
     print 'Jungle Checkers ranking'
 
+    global N, K
     N = 47 # Every digit in range [0, N-1]
     K = 3 # Permutation length
 
@@ -168,34 +193,77 @@ def main():
         fac[i] = i * fac[i-1]
 
 
+    bla = [43, 4, 0]
+    #bla = [0, 1, 2]
+    bla_index = [0, 0, 0]
+    #perm_to_index(N, K, bla, bla_index)
+    #print bla_index
+    #perm_by_index(N, K, bla, bla_index)
+    #print bla
+    #print rank_by_index(N, K, bla_index)
+    #print rank(N, K, bla)
+
     perm = range(0, K)
     perm_index = [0]*K
         
     # Loop thru all permutations and test rank/unrank
     #for index in range(0, factorial[K]-1):
-    index = 0
-    while index < factorial[K]-1:
-    #while index < 1:
-    #for index in range(0, 100):
-    #for index in range(0, fac[N]/fac[N-K]-1):
+    i = 0
+    r = 0
+    while i < factorial[K]-1:
+
+    #while i < 100:
+        r = index_to_rank(N, K, perm_index)
+        if r != i:
+            print r, i
+            break
+        increment_index(N, K, perm_index)
+
+        
         #perm = [-1] * K
+        
         #unrank(N, K, index, perm)
-        print perm 
-        #r = rank(N, K, perm)
-        next_rank(N, K, perm, perm_index)
-        """
-        print index, '\t', perm, '\t', r
-        if index != r:
-            print index, '\t', perm, '\t', r
-            break
-        if len(perm) != len(set(perm)):
-            print index, '\t', perm, '\t', r
-            break
-        """
+
+        #perm2 = perm[:]
+
+
+        #r = rank(N, K, perm2) # this changes perm
+        #perm2 = [0]*K 
+        #perm_by_index(N, K, perm2, perm_index)
+        
+
+        #print i, perm2, perm_index, rank_by_index(N, K, perm_index), perm2, rank(N, K, perm2),
+
+        #perm3 = perm[:]
+        #next_index(N, K, perm_index)
+        #perm3 = [0]*K
+        #perm_by_index(N, K, perm3, perm_index)
+        #print perm3
+        
+
+        #print perm, index, r, perm3
+        i += 1
+        #print perm 
         #print index, '\t', perm, '\t', r
-        index += 1
+        #if index != r:
+        #    print index, '\t', perm, '\t', r
+        #    break
+        #print index, '\t', perm, '\t', r
+
     #print perm
-    print index
+    #print i, perm_index, rank_by_index(N, K, perm_index), perm2, rank(N, K, perm2),
+    
+    """
+    bla = [1, 8, 10, 9]
+    bla_index = [0, 0, 0, 0]
+    index_by_perm(N, K, bla, bla_index)
+    #print bla_index
+    print rank_by_index(N, K, bla_index)
+    perm_by_index(N, K, bla, bla_index)
+    print bla
+    print rank(N, K, bla)
+    """
+
     """
 
     index = 1
@@ -218,6 +286,19 @@ def main():
     """
 
 
+    perm = [42, 0, 4]
+    perm_index = [0, 0, 0]
+    perm_to_index(N, K, perm, perm_index)
+    print perm_index
+    index_to_perm(N, K, perm, perm_index)
+    print perm
+    print index_to_rank(N, K, perm_index)
+    print rank(N, K, perm)
+    unrank(N, K, 2416, perm)
+    print perm
+    print index_to_rank(N, K, perm_index)
+    rank_to_index(2416, perm_index)
+    print perm_index
 
 
     print 'done'
